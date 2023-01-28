88257f2d92f3077797f44ecc70da2f3fbc40b9ac
Krystal Kimmel <kabkimmel@gmail.com>
2023-01-28 14:55:12 -0600

    solved problem 00

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,8 +12,18 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
+  let sum = 0;
+
+  arr.forEach(function(value) {
+    sum += value;
+  });
+
+  return sum;
 };
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
