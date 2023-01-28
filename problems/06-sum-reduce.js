d53752a66f3b3a3b4157de085802379a643e9bb3
ian <iankimmmmm@gmail.com>
2023-01-28 13:58:54 -0800

    solved problem 06

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -13,7 +13,11 @@ console.log(sumWithReduce([])); // 0
 */
 
 let sumWithReduce = function(nums) {
-  // Your code here
+  if(nums.length === 0) return 0;
+  let sum = nums.reduce(function(accum, ele) {
+    return accum + ele;
+  })
+  return sum;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
