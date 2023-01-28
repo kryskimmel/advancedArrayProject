bd805f08c2366cebbb67770e0ee0ef373db8361a
Krystal Kimmel <kabkimmel@gmail.com>
2023-01-28 15:14:52 -0600

    completed problem 03

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -14,8 +14,13 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 */
 
 let longWords = function(words) {
-    // Your code here
+    return words.filter(function(value){
+      if (value.length > 5) return true;
+    });
 };
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -23,4 +28,4 @@ try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}
