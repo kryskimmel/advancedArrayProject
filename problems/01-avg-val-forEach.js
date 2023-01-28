67296e90fcdd21cbc87660f6436e738b6878be9d
Krystal Kimmel <kabkimmel@gmail.com>
2023-01-28 15:01:04 -0600

    completed problem 01

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -14,8 +14,20 @@ console.log(avgVal([])); // null
 */
 
 let avgVal = function(arr) {
-    // Your code here
+    if (arr.length === 0) return null;
+    else {
+      let sum = 0;
+      arr.forEach(function(value) {
+        sum += value;
+      });
+      return sum / arr.length;
+    }
+
 };
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
