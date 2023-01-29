b5606e88ba49bd80b70750a33639e466ff894fa6
Krystal Kimmel <kabkimmel@gmail.com>
2023-01-28 21:09:15 -0600

    completed problem 18

--- problems/18-two-d-product.js
+++ problems/18-two-d-product.js
@@ -26,8 +26,35 @@ console.log(twoDimensionalProduct(arr2)); // 88
 */
 
 let twoDimensionalProduct = function(arr) {
-    // Your code here
+  let product = 1;
+
+    arr.forEach(function(value){
+
+      value.forEach(function(el){
+      product *= el;
+      });
+
+    });
+    return product;
 };
+
+
+
+
+
+let arr1 = [
+  [6, 4],
+  [5],
+  [3, 1]
+];
+console.log(twoDimensionalProduct(arr1)); // 360
+
+let arr2 = [
+  [11, 4],
+  [2]
+];
+console.log(twoDimensionalProduct(arr2)); // 88
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
