d86b0c23f1af8c1b7e4c39964012e02fb808b741
Krystal Kimmel <kabkimmel@gmail.com>
2023-01-28 20:08:18 -0600

    completed problem 16

--- problems/16-hipsterfy.js
+++ problems/16-hipsterfy.js
@@ -17,7 +17,7 @@ console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls
 let removeLastVowel = function(word) {
   let words = word.split("");
   let vowels = 'aeiouAEIOU';
-  for(let i = words.length - 1; i > 0; i--){
+  for(let i = words.length - 1; i >= 0; i--){
     if(vowels.includes(words[i])){
         words.splice(i, 1).join("")
         return words.join("");
@@ -25,18 +25,25 @@ let removeLastVowel = function(word) {
   }
 };
 
-let hipsterfy = function(sentence) {
-  let split = sentence.split(" ");
-  split.forEach(function(value){
-    value = removeLastVowel(value);
-  })
-  return split.join(" ");
-};
+
+
+// let hipsterfy = function(sentence) {
+//   let split = sentence.split(" ");
+//   split.forEach(function(value){
+//     value = removeLastVowel(value);
+//   })
+//   return split.join(" ");
+// };
 
 // alternative solution using Array.map
-// let hipsterfy = function(sentence) {
-//     return sentence.split(' ').map(removeLastVowel).join(' ');
-// };
+let hipsterfy = function(sentence) {
+    return sentence.split(' ').map(removeLastVowel).join(' ');
+};
+
+
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
